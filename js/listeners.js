d67426edb27b6030ae16dfb578761bf8bdb338d3
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-11 13:10:33 -0400

    jquery listeners removed elements completely

--- js/listeners.js
+++ js/listeners.js
@@ -1,39 +1,24 @@
-import * as elements from "./elements.js";
 import * as handler from "./handlers.js";
 
 const addListeners = () => {
-    // Iterating objects through method learned in url below:
-    // https://www.freecodecamp.org/news/how-to-iterate-over-objects-in-javascript/
-    // Red button listeners
-    let redButtons = Object.values(elements.red);
-    redButtons.forEach((element) => {
-        $(element).click( () => {
-            handler.red(element.innerHTML);
-        });
-    })
-
-    // Yellow button listeners
-    let yellowButtons = Object.values(elements.yellow);
-    yellowButtons.forEach((element) => {
-        $(element).click( () => {
-            handler.yellow(element.innerHTML);
-        });
-    })
-
-    // Green button listeners
-    let greenButtons = Object.values(elements.green);
-    greenButtons.forEach((element) => {
-        $(element).click( () => {
-            handler.green(element.innerHTML);
-        });
-    })
-
-    // Blue button listeners
-    let blueButtons = Object.values(elements.blue);
-    blueButtons.forEach((element) => {
-        $(element).click( () => {
-            handler.blue(element.innerHTML);
-        });
+    let colors = ['red', 'yellow', 'green', 'blue']
+    
+    colors.forEach( (color) => {
+        buttons = document.querySelectorAll(`.${color} > button`)
+        buttons.forEach( (button) => {
+            $(button).click( () => {
+                switch (color) {
+                    case 'red':
+                        handler.red(button.innerHTML)
+                    case 'yellow':
+                        handler.yellow(button.innerHTML)
+                    case 'green':
+                        handler.green(button.innerHTML)
+                    case 'blue':
+                        handler.blue(button.innerHTML)
+                }
+            })
+        })
     })
 
     // Listeners for game flow control
